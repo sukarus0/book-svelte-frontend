281bb90931d5500560d281fd7e0f39d1bf051b02
sukarus0 <sukarus@gamil.com>
2022-07-22 06:56:42 +0000

    Done 4-4-2

--- 4-4/src/App.svelte
+++ 4-4/src/App.svelte
@@ -3,14 +3,14 @@
 </script>
 
 <Card>
-	<h2>안녕하세요!</h2>
-	<p>이곳은 내용이 들어가는 영역입니다.</p>
-</Card>
+	<span slot="name">
+		홍길동
+	</span>
 
-<Card>
-	<h2>Hello Svelte</h2>
-	<p>Svelte를 배우는 여러분 환경합니다.</p>
-	<p>즐거운 코딩 되세요..</p>
+	<span slot="address">
+		서울특별시<br>
+		여의도동
+	</span>
 </Card>
 
 <style>
--- 4-4/src/card.svelte
+++ 4-4/src/card.svelte
@@ -7,8 +7,39 @@
 		padding: 1em;
 		margin: 0 0 1em 0;
 	}
+
+	h2 {
+		padding: 0 0 0.2em 0;
+		margin: 0 0 1em 0;
+		border-bottom: 1px solid #ff3e00;
+	}
+
+	.address, .email {
+		padding: 0 0 0 1.5em;
+		background: 0 50% no-repeat;
+		background-size: 1em 1em;
+		margin: 0 0 0.5em 0;
+		line-height: 1.2;
+	}
+
 </style>
 
-<div class="card">
-	<slot></slot>
-</div>
+<article class="contact-card">
+	<h2>
+		<slot name="name">
+			<span class="missing"> 이름 미입력 </span>
+		</slot>
+	</h2>
+
+	<div class="address">
+		<slot name="address">
+			<span class="missing"> 주소 미입력 </span>
+		</slot>
+	</div>
+
+	<div class="email">
+		<slot name="email">
+			<span class="missing"> 이메일 미입력 </span>
+		</slot>
+	</div>
+</article>

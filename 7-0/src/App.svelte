f8394cd4a94353137ce023dd55777b54799f4e94
sukarus0 <sukarus@gamil.com>
2022-07-27 04:31:31 +0000

    Done 7-0

---
+++ 7-0/src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+	import Modal from './modal.svelte'
+	let modalActive1 = false
+	let modalActive2 = false
+</script>
+
+<Modal bind:modalActive={modalActive1}>
+	<h2 slot="header">
+		MODAL POPUP TITLE 1
+	</h2>
+	<p slot='content'>
+		MODAL POPUP <b> CONTENT 1 </b>
+	</p>
+</Modal>
+
+<Modal bind:modalActive={modalActive2}>
+	<h2 slot='header'>
+		MODAL POPUP TITLE 2
+	</h2>
+	<p slot='content'>
+		MODAL POPUP CONTENT 2
+	</p>
+</Modal>
+
+<button on:click={()=>modalActive1 = true}>
+	Modal PopUp 1
+</button>
+
+<button on:click={()=>modalActive2 = true}>
+	Modal PopUp 2
+</button>
+
+<div>
+	<p>modalActive1: {modalActive1}</p>
+	<p>modalActive2: {modalActive2}</p>
+</div>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>

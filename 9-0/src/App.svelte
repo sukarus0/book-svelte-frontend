97dbf1eaa5b29eee86ffe12c601665384cd5a3ea
sukarus0 <sukarus@gamil.com>
2022-08-08 06:52:47 +0000

    TEST base tinro

--- 9-0/src/App.svelte
+++ 9-0/src/App.svelte
@@ -1,30 +1,16 @@
 <script>
-	export let name;
+	import { Route } from 'tinro';
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<nav>
+	<a href='/'> HOME </a>
+	<a href='/about'> ABOUT </a>
+	<a href='/contacts'> CONTACTS </a>
+</nav>
+
+<Route path='/'><h1>Home Page</h1></Route>
+<Route path='/about'><h1>About Page</h1></Route>
+<Route path='/contacts'><h1>Contacts Page</h1></Route>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
